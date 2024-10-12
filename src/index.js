c19ece0cb82ef684fff4f8724e8a3ea7e292a2f3
TheNofis <matbey122@gmail.com>
2024-10-12 13:28:39 +0300

    remove commits

--- src/index.js
+++ src/index.js
@@ -31,37 +31,3 @@ const initHandler = new InitHandler({ io });
 initHandler.init();
 
 server.listen(3001);
-
-// io.on("connection", (socket) => {
-//   socket.on("registerClient", (clientId) => {
-//     if (clientId == null) return;
-//     try {
-//       const client = connections.newConection({ socket });
-//
-//       socket.emit("connectInfo", {
-//         clientId: client.getClientId(),
-//         creator: client.isCreator(),
-//       });
-//       console.log(`clientConnect: ${clientId}`);
-//     } catch (error) {
-//       console.log(`clientConnect ERROR: ${clientId} ${error}`);
-//     }
-//   });
-//
-//   socket.on("setUsername", (username) => {
-//     connections.getConnection(socket.id)?.setUsername(username);
-//     socket.emit("userData", {
-//       username: connections.getConnection(socket.id).getUsername(),
-//       creator: connections.getConnection(socket.id).isCreator(),
-//     });
-//     io.emit("userList", connections.getUsers());
-//     socket.emit("filesContent", getFilesNames(connections.getFilesContent()));
-//   });
-//
-//   socket.on("disconnect", () => {
-//     connections.deleteConnection(socket.id);
-//     io.emit("userList", connections.getUsers());
-//   });
-// });
-//
-//
